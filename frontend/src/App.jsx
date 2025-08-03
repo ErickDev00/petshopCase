ce4f61270601fc895cf0a104962cf4f32b06943c
Erick <profissional.erick@gmail.com>
2025-08-03 01:05:25 -0300

    WIP: implementação inicial do consumo da API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,53 @@
 import "./App.css";
 
 function App() {
+  async function enviarForm(e) {
+    e.preventDefault();
+
+    const form = e.target;
+    const data = form.data.value;
+    const qtdCaesPequenos = form.qtdCaesPequenos.value;
+    const qtdCaesGrandes = form.qtdCaesGrandes.value;
+
+    const url = new URL("http://localhost:8080/petshop/melhor-opcao");
+    url.searchParams.append("data", data);
+    url.searchParams.append("qtdCaesPequenos", qtdCaesPequenos);
+    url.searchParams.append("qtdCaesGrandes", qtdCaesGrandes);
+
+    try {
+      const response = await fetch(url, { method: "POST" });
+      const resultado = await response.json();
+
+      alert(
+        `Melhor Petshop: ${
+          resultado.nome
+        } - Preço: R$ ${resultado.precoTotal.toFixed(2)}`
+      );
+    } catch (erro) {
+      console.error("Erro:", erro);
+    }
+  }
+
   return (
     <>
       <div>
         <div>
-          <form>
+          <form onSubmit={enviarForm}>
             <label>
               Data:
-              <input type="date" name="data" id="data" />
+              <input type="date" name="data" id="data" required />
             </label>
             <br />
 
             <label>
               Quantidade de cães pequenos:
-              <input type="number" name="qtdCaesPequenos" />
+              <input type="number" name="qtdCaesPequenos" required />
             </label>
             <br />
 
             <label>
               Quantidade de cães grandes:
-              <input type="number" name="qtdCaesGrandes" />
+              <input type="number" name="qtdCaesGrandes" required />
             </label>
             <br />
 
