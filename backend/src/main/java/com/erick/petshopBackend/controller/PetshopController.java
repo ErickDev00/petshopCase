c7a4b05a5f9e5b459899fca60064df3ee3a1284f
Erick <profissional.erick@gmail.com>
2025-08-03 18:13:32 -0300

    feat(controller): adicionar as validações dos dados

--- backend/src/main/java/com/erick/petshopBackend/controller/PetshopController.java
+++ backend/src/main/java/com/erick/petshopBackend/controller/PetshopController.java
@@ -22,6 +22,20 @@ public class PetshopController {
     @PostMapping("/melhor-opcao")
     public ResponseEntity<?> obterMelhor(@RequestBody PetshopDTO dto) {
 
+        //verificações para evitar erros
+        if (dto.getData() == null || dto.getData().isBefore(LocalDate.now())) {
+            return ResponseEntity.badRequest().body("Data inválida. Deve ser hoje ou futura.");
+        }
+
+        if (dto.getQtdCaesPequenos() < 0 || dto.getQtdCaesGrandes() < 0) {
+            return ResponseEntity.badRequest().body("Quantidade de cães não pode ser negativa.");
+        }
+
+        if (dto.getQtdCaesPequenos() + dto.getQtdCaesGrandes() < 1) {
+            return ResponseEntity.badRequest().body("Informe pelo menos um cão.");
+        }
+
+
         Petshop melhor = petshopService.melhorPetshop(dto.getData(), dto.getQtdCaesPequenos(), dto.getQtdCaesGrandes());
 
         return ResponseEntity.ok(melhor);
