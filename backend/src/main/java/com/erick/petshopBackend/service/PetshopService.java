5b25b750e3549057f196ab61d83a2d65624cf14d
Erick <profissional.erick@gmail.com>
2025-08-03 23:23:39 -0300

    feat: stream no lugar do metodo de comparaçcao

--- backend/src/main/java/com/erick/petshopBackend/service/PetshopService.java
+++ backend/src/main/java/com/erick/petshopBackend/service/PetshopService.java
@@ -5,19 +5,17 @@ import org.springframework.stereotype.Service;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class PetshopService {
 
     public Petshop melhorPetshop(LocalDate data, int qtdCaesPequenos, int qtdCaesGrandes){
 
+        Petshop meuCaninoFeliz = new Petshop ("Meu Canino Feliz", 2.0, 20.00, 40.00, 0.2, 0.2);
+        Petshop vaiRex = new Petshop("Vai Rex", 1.7, 15.00, 50.00, 0.2, 0.1);
+        Petshop chowChawgas = new Petshop("ChowChawgas", 0.80, 30.00, 45.00, 0, 0);
 
-        //instanciando PetShops
-        Petshop meuCaninoFeliz = new Petshop ("Meu Canino Feliz", 2.0, 20.00, 40.00);
-        Petshop vaiRex = new Petshop("Vai Rex", 1.7, 15.00, 50.00);
-        Petshop chowChawgas = new Petshop("ChowChawgas", 0.80, 30.00, 45.00);
 
         List<Petshop> petshops = new ArrayList<>();
         petshops.add(meuCaninoFeliz);
@@ -25,46 +23,26 @@ public class PetshopService {
         petshops.add(chowChawgas);
 
 
-        //verificação fim de semana
+        return petshops.stream()
+                .min(Comparator.comparingDouble((Petshop p) -> calcularPreco(data, qtdCaesPequenos, qtdCaesGrandes, p))
+                        .thenComparingDouble(Petshop::getDistancia))
+                .orElse(null);
+    }
+
+
+    private Double calcularPreco(LocalDate data, int qtdCaesPequenos, int qtdCaesGrandes, Petshop petshop){
+
         boolean fimDeSemana = data.getDayOfWeek() == DayOfWeek.SATURDAY ||
                 data.getDayOfWeek() == DayOfWeek.SUNDAY;
 
+
         if (fimDeSemana){
-            meuCaninoFeliz.setPrecoCaoPequeno(24.00);
-            meuCaninoFeliz.setPrecoCaoGrande(48.00);
-            vaiRex.setPrecoCaoPequeno(20.00);
-            vaiRex.setPrecoCaoGrande(55.00);
+            return qtdCaesPequenos * petshop.getPrecoCaoPequeno() * (1 + petshop.getAcrescimoCachorroPequeno()) +
+                    qtdCaesGrandes * petshop.getPrecoCaoGrande() * (1 + petshop.getAcrescimoCachorroGrande());
+        } else {
+            return qtdCaesPequenos * petshop.getPrecoCaoPequeno()  +
+                    qtdCaesGrandes * petshop.getPrecoCaoGrande();
         }
-
-
-        //calculando preco para cada petshop
-        for (Petshop p: petshops){
-            double precoTotal = qtdCaesPequenos * p.getPrecoCaoPequeno() + qtdCaesGrandes * p.getPrecoCaoGrande();
-            p.setPrecoTotal(precoTotal);
-        }
-
-
-        return comparandoPetshop(petshops);
-
     }
-
-
-    public Petshop comparandoPetshop(List<Petshop> petshops){
-        if (petshops == null || petshops.isEmpty()) return null; //tentar retornar um erro para o usuário.
-        Petshop melhor = petshops.get(0);
-
-        for (Petshop p : petshops){
-            if (p.getPrecoTotal() < melhor.getPrecoTotal()){
-                melhor = p;
-            } else if (p.getPrecoTotal() == melhor.getPrecoTotal() && p.getDistancia() < melhor.getDistancia()){
-                melhor = p;
-            }
-        }
-
-        return melhor;
-
-    }
-
-
 
 }
